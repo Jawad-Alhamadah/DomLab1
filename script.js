8e5d0c112753ba497eaba04edb388e5cbf97ddb6
vortix2950 <vortix2950@ksu.edu>
2024-09-11 11:38:29 +0300

    javascript DOM demo

---
+++ script.js
@@ -0,0 +1,105 @@
+
+
+let title = document.getElementsByClassName("main-title")[0]
+
+title.style.color="#ffcd12"
+title.style.backgroundColor="#abcd23"
+
+
+let paragraph = document.getElementById("main-paragraph")
+
+
+paragraph.style.fontSize="40px"
+paragraph.classList.add("text")
+
+
+let img = document.getElementsByTagName("img")[0]
+img.src="pexels-photo-460775.jpeg"
+img.style.width="200px"
+
+let item = document.createElement("li")
+item.style.display="inline"
+item.style.color="green"
+item.style.display="inline"
+
+item.style.border="1px solid black"
+item.innerText="list item"
+
+let list = document.querySelector("ul")
+list.append(item)
+
+
+item = document.createElement("li")
+item.style.display="inline"
+item.style.color="blue"
+ 
+
+item.style.border="1px solid black"
+item.innerText="list item"
+list.append(item)
+
+
+item = document.createElement("li")
+item.style.display="inline"
+item.style.color="purple"
+ 
+
+item.style.border="1px solid black"
+item.innerText="list item"
+list.append(item)
+
+
+let table = document.createElement("table")
+table.classList.add("colored_table")
+
+let row = document.createElement("tr")
+
+
+let data_cell = document.createElement("td")
+data_cell.innerText= "data"
+
+row.appendChild(data_cell)
+
+
+data_cell = document.createElement("td")
+data_cell.innerText= "data"
+row.appendChild(data_cell)
+
+data_cell = document.createElement("td")
+data_cell.innerText= "data"
+row.appendChild(data_cell)
+
+table.appendChild(row)
+/////////////////////////////////////
+
+
+
+row = document.createElement("tr")
+
+
+data_cell = document.createElement("td")
+data_cell.innerText= "data"
+
+row.appendChild(data_cell)
+
+
+data_cell = document.createElement("td")
+data_cell.innerText= "data"
+row.appendChild(data_cell)
+
+data_cell = document.createElement("td")
+data_cell.innerText= "data"
+row.appendChild(data_cell)
+
+table.appendChild(row)
+
+
+document.body.appendChild(table)
+
+// let col = document.createElement("tl")
+// col.innerText="im row"
+// table.appendChild(row)
+// row = document.createElement("tr")
+// table.appendChild(row)
+// row = document.createElement("tr")
+// table.appendChild(row)
